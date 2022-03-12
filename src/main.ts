c6bef69df75b3f9e4975743585bf2cdcec2b7c67
James_Calvo <15jcalvo@gmail.com>
2022-03-12 00:07:23 -0500

    added dotenv

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import 'dotenv/config'
 
 const port = process.env.PORT || 9000
 
